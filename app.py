db748d5c6875aebade5064e7ac96c04b3615b13f
Mao-Mizoguchi <m.mizoguchi.sys21@morijyobi.ac.jp>
2022-03-22 10:30:40 +0900

    円の円の面積計算機能追加

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(_name_)
 
@@ -7,3 +7,13 @@ def top_page():
     return render_template("index.html")
 if_name_ == " _main_":
     app.run(debug=True)
+
+@app route("/circle_input")
+def circle_input():
+    return render_template("circle_input.html")
+
+@app.route("/circle_result")
+def cicle_result():
+    radius = int(request.args.get("radius"))
+    result = 3.14 * radius ** 2
+    return render_template("cicle_result.html",result=result)
